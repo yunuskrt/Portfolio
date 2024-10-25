e29677a23d02508f441489a846c152f699308520
Yunus <ykerestecioglu@sabanciuniv.edu>
2024-10-25 23:19:39 +0300

    contact page & contact form & form validation

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -1,8 +1,96 @@
-import React from 'react'
+import React, { useState, useEffect, useRef } from 'react'
+import GLOBE from 'vanta/dist/vanta.globe.min'
+import * as THREE from 'three'
+import { sendMail, submitContactForm, clearContactForm } from '@/utils/helpers'
 import styles from './contact.module.css'
 
 const Contact = () => {
-	return <div>Contact</div>
-}
+	const [vantaEffect, setVantaEffect] = useState(0)
+	const vantaRef = useRef(null)
 
+	const nameRef = useRef(null)
+	const emailRef = useRef(null)
+	const commentRef = useRef(null)
+
+	useEffect(() => {
+		if (!vantaEffect) {
+			setVantaEffect(
+				GLOBE({
+					el: vantaRef.current,
+					THREE: THREE,
+					mouseControls: true,
+					touchControls: true,
+					gyroControls: false,
+					minHeight: 200.0,
+					minWidth: 200.0,
+					scale: 1.0,
+					scaleMobile: 1.0,
+				})
+			)
+		}
+		return () => {
+			if (vantaEffect) vantaEffect.destroy()
+		}
+	}, [vantaEffect])
+
+	const handleFormSubmit = (e) => {
+		e.preventDefault()
+		const validForm = submitContactForm(nameRef, emailRef, commentRef)
+		if (validForm) {
+			sendMail(nameRef, emailRef, commentRef)
+			clearContactForm(nameRef, emailRef, commentRef)
+		} else {
+			console.log('form is not valid')
+		}
+	}
+	return (
+		<div ref={vantaRef} style={{ height: '100vh', width: '100%' }}>
+			<div
+				style={{
+					height: '100%',
+					width: '50%',
+					display: 'flex',
+					justifyContent: 'flex-end',
+					alignItems: 'center',
+				}}
+			>
+				<form className={styles.contactForm} onSubmit={handleFormSubmit}>
+					<h1 className={styles.contactTitle}>Contact</h1>
+					<div className='form-group'>
+						<input
+							name='name'
+							type='text'
+							className={styles.feedbackInput}
+							placeholder='Name'
+							ref={nameRef}
+						/>
+					</div>
+					<div className='form-group'>
+						<input
+							name='email'
+							type='text'
+							className={styles.feedbackInput}
+							placeholder='Email'
+							ref={emailRef}
+						/>
+					</div>
+					<div className='form-group'>
+						<textarea
+							name='text'
+							id='comment'
+							className={styles.feedbackInput}
+							placeholder='Comment'
+							ref={commentRef}
+						></textarea>
+					</div>
+					<input
+						type='submit'
+						value='SUBMIT'
+						className={styles.submitFormBtn}
+					/>
+				</form>
+			</div>
+		</div>
+	)
+}
 export default Contact
--- utils/helpers.js
+++ utils/helpers.js
@@ -11,3 +11,63 @@ export const scrollIntoView = (sectionId) => {
 		})
 	}
 }
+
+// Function to display error messages of form fields
+const showError = (field, errorText) => {
+	field.current.classList.add('error')
+	const errorElement = document.createElement('small')
+	errorElement.classList.add('error-text')
+	errorElement.innerText = errorText
+	field.current.closest('.form-group').appendChild(errorElement)
+}
+
+// Function to handle form submission of contact form
+export const submitContactForm = (nameRef, emailRef, commentRef) => {
+	// Getting trimmed values from input fields
+	const name = nameRef.current.value.trim()
+	const email = emailRef.current.value.trim()
+	const comment = commentRef.current.value.trim()
+	// Regular expression pattern for email validation
+	const emailPattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/
+	// Clearing previous error messages
+	document
+		.querySelectorAll('.form-group .error')
+		.forEach((field) => field.classList.remove('error'))
+	document
+		.querySelectorAll('.error-text')
+		.forEach((errorText) => errorText.remove())
+	// Performing validation checks
+
+	let validForm = true
+	if (name === '') {
+		showError(nameRef, 'Enter your full name')
+		validForm = false
+	}
+	if (!emailPattern.test(email)) {
+		showError(emailRef, 'Enter a valid email address')
+		validForm = false
+	}
+	if (comment === '') {
+		showError(commentRef, 'Enter your comment')
+		validForm = false
+	}
+	return validForm
+	// Checking for any remaining errors before form submission
+	// const errorInputs = document.querySelectorAll('.form-group .error')
+	// if (errorInputs.length > 0) return
+}
+
+export const clearContactForm = (nameRef, emailRef, commentRef) => {
+	nameRef.current.value = ''
+	emailRef.current.value = ''
+	commentRef.current.value = ''
+}
+
+export const sendMail = async (nameRef, emailRef, commentRef) => {
+	// Getting trimmed values from input fields
+	const name = nameRef.current.value.trim()
+	const email = emailRef.current.value.trim()
+	const comment = commentRef.current.value.trim()
+	// Sending email to the server
+	console.log({ name, email, comment })
+}
